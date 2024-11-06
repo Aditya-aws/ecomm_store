81e6c60aa53a267fdd127dde552f0afe81ab307e
Aditya-aws <adityajaiswal6712@gmail.com>
2024-11-06 15:53:27 +0530

    Update constants.ts
    LBR

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a424f16e5fc8e413e99625960344c0ed-1743244476.us-east-1.elb.amazonaws.com',
 };
